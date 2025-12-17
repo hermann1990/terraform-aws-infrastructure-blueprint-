5061a8b7778d9bccabba22ac52801345c11435e4
hermann1990 <76397203+hermann1990@users.noreply.github.com>
2025-12-17 13:34:44 -0500

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "Public subnet CIDR"
+  type        = string
+}
