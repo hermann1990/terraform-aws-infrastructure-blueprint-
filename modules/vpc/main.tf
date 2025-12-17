433cc5b9f738401c1e1c1f2509d47ee31e145fac
hermann1990 <76397203+hermann1990@users.noreply.github.com>
2025-12-17 13:34:08 -0500

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
